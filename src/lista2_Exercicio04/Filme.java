ce3869f55a418ff67023bc669b533152aa24bc44
Usuario <Usuario@192.168.10.47>
2018-05-10 13:14:24 -0300

    Adicionando um método ao exercicio

--- src/lista2_Exercicio04/Filme.java
+++ src/lista2_Exercicio04/Filme.java
@@ -1,5 +1,20 @@
 package lista2_Exercicio04;
 
 public class Filme {
+	
+	//Atributos
+	int excelente = 0, otimo = 0, bom = 0, regular = 0, ruim = 0;
 
+	//Método - Menu de avaliação
+	public String menuAvaliacao() {
+		
+		//Variveis
+		String menu = "**** Menu de avaliação ****\n\n";
+		
+		
+		
+		
+		return menu;
+	}
+	
 }
